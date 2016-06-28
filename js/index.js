d18bfcfdee1e7fe2a968ecfe761d1341714de04c
el-ROSSO <rosshagino@icloud.com>
2016-06-27 14:25:28 -1000

    added frogs
    frog memes

---
+++ js/index.js
@@ -0,0 +1,27 @@
+
+var colors = [
+	"red", 
+	"orange", 
+	"yellow", 
+	"green", 
+	"indigo", 
+	"blue", 
+	"violet"];
+
+var count = 0;
+
+	colors[4];
+
+var cycle = function(array) {
+	console.log(colors[count]);
+	count++;
+	if (count == array.length) {
+		count = 0;
+	} else {
+
+	};
+};
+
+var display = function () {
+	cycle(colors);
+};
